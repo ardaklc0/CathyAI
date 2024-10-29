3e083865482accecf3ad26fcc80c5cf02a3165df
arda <ardaklc0@gmail.com>
2024-10-29 18:46:18 +0300

    enter django (metallica)

---
+++ djangotutorial/polls/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
